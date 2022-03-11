c58f2069972ab1e1df0aa9f19119db6129121473
Magnus H <me@magnushodne.com>
2022-03-11 11:35:28 +0100

    Created a button instead

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  Image,
-  SafeAreaView,
-} from "react-native";
+import { StyleSheet, Button, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>This is my new, awesome app!</Text>
-      <TouchableHighlight onPress={() => console.log("The image was tapped")}>
-        <Image
-          blurRadius={2}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight>
-      <StatusBar style="auto" />
+      <Button
+        color={"orange"}
+        title={"Click me!"}
+        onPress={() => console.log("Button tapped")}
+      />
     </SafeAreaView>
   );
 }
