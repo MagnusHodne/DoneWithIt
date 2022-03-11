fa1ff718e75268a08a958539b88d28db056f8226
Magnus H <me@magnushodne.com>
2022-03-11 11:32:34 +0100

    Added TouchableComponent wrapper to image

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>This is my new, awesome app!</Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableHighlight onPress={() => console.log("The image was tapped")}>
+        <Image
+          blurRadius={2}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
