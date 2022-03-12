eebd8503cfa040a1da081d6d410c6484cc11d9dc
Magnus H <me@magnushodne.com>
2022-03-12 22:31:59 +0100

    Reverted to pure portait app

--- App.js
+++ App.js
@@ -6,20 +6,14 @@ import {
   Platform,
 } from "react-native";
 
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
-
 export default function App() {
-  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: "100%",
-          height: landscape ? "100%" : "30%",
+          height: "30%",
           alignSelf: "center",
         }}
       ></View>
