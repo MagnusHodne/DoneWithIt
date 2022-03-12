5ffd2b928bb1ff8f34624b93522dda812e2cd347
Magnus H <me@magnushodne.com>
2022-03-12 22:31:06 +0100

    Example video player that fills screen in landscape

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ import {
   Platform,
 } from "react-native";
 
-import { useDimensions } from "@react-native-community/hooks";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
-  console.log(useDimensions());
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "75%",
-          height: "30%",
+          width: "100%",
+          height: landscape ? "100%" : "30%",
           alignSelf: "center",
         }}
       ></View>
