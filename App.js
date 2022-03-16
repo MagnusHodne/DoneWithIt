b4aea70ee12efd97bbf29f640e6731ac4c6607f0
Magnus H <me@magnushodne.com>
2022-03-16 14:14:10 +0100

    Text styling

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Text, View, Platform } from "react-native";
 
 export default function App() {
   return (
@@ -9,18 +9,24 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          shadowColor: "grey",
-          shadowOffset: { width: 5, height: 10 },
-          shadowOpacity: 1,
-          shadowRadius: 10,
-          elevation: 10 /*This is the only shadow property on Android - it is less customizable*/,
+          color: "olive",
+          fontFamily: Platform.OS === "ios" ? "Courier" : "Roboto",
+          fontStyle: "italic",
+          fontWeight: "300",
+          fontSize: 30,
+          textAlign: "center",
+          textTransform: "capitalize",
         }}
-      />
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        consectetur esse exercitationem facere harum magnam modi nesciunt odit
+        placeat, repellat? A architecto blanditiis consequatur fugit laborum
+        maiores, minus odit, provident reiciendis sed temporibus totam velit.
+        Enim possimus reiciendis reprehenderit? Assumenda cum doloremque eaque
+        eveniet in non optio quam quas sequi!
+      </Text>
     </View>
   );
 }
