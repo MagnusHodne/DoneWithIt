3f2459a1624740c53011e6bd76a6b99bf2c2a3bd
Magnus H <me@magnushodne.com>
2022-03-16 14:30:49 +0100

    A cleaner approach to deal with platform specific styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppText from "./app/components/text/AppText";
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <MaterialCommunityIcons name={"email"} size={60} color={"gold"} />
+      <AppText>This text is platform specific!</AppText>
     </View>
   );
 }
