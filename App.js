b458d165f0edb58ff9581f7236b206190719d326
Magnus H <me@magnushodne.com>
2022-03-16 14:23:50 +0100

    Encapsulating Styles (how to get the same text styling in multiple places)

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { Text, View, Platform } from "react-native";
+import AppText from "./app/components/text/AppText";
 
 export default function App() {
   return (
@@ -9,24 +10,9 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text
-        style={{
-          color: "olive",
-          fontFamily: Platform.OS === "ios" ? "Courier" : "Roboto",
-          fontStyle: "italic",
-          fontWeight: "300",
-          fontSize: 30,
-          textAlign: "center",
-          textTransform: "capitalize",
-        }}
-      >
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-        consectetur esse exercitationem facere harum magnam modi nesciunt odit
-        placeat, repellat? A architecto blanditiis consequatur fugit laborum
-        maiores, minus odit, provident reiciendis sed temporibus totam velit.
-        Enim possimus reiciendis reprehenderit? Assumenda cum doloremque eaque
-        eveniet in non optio quam quas sequi!
-      </Text>
+      <AppText>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, odit.
+      </AppText>
     </View>
   );
 }
