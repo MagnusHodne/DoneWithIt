f3569fc99da548d454da27a3001297e8f3548a7e
Magnus H <me@magnushodne.com>
2022-03-16 14:04:35 +0100

    Shadows on iOS

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImage from "./app/screens/ViewImage";
+import { View } from "react-native";
 
 export default function App() {
-  return <ViewImage />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          shadowColor: "grey",
+          shadowOffset: { width: 5, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+        }}
+      />
+    </View>
+  );
 }
