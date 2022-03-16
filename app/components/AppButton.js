14673718893d56ffd6fbc8dd1acd9357883bb7c8
Magnus H <me@magnushodne.com>
2022-03-16 15:46:13 +0100

    Cleaned up style overriding

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, TouchableHighlight } from "react-native";
-function AppButton({ children, onPress, color, width }) {
-  const styles = StyleSheet.create({
-    button: {
-      backgroundColor: color ? color : "grey",
-      borderRadius: 30,
-      padding: 15,
-      margin: 5,
-      width: width ? width : "100%",
-    },
-    text: {
-      fontSize: 18,
-      color: "white",
-      fontWeight: "bold",
-      textAlign: "center",
-    },
-  });
+import colors from "../config/colors";
+function AppButton({ title, onPress, color = "primary" }) {
   return (
     <TouchableHighlight
       activeOpacity={0.3}
       underlayColor={color}
       onPress={onPress}
-      style={styles.button}
+      style={[styles.button, { backgroundColor: color }]}
     >
-      <Text style={styles.text}>{children}</Text>
+      <Text style={styles.text}>{title}</Text>
     </TouchableHighlight>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 30,
+    padding: 15,
+    margin: 5,
+    width: "100%",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default AppButton;
--- app/components/AppText.ios.js
+++ app/components/AppText.ios.js
@@ -3,14 +3,15 @@ import { StyleSheet, Text } from "react-native";
 
 import texts from "../config/texts";
 
-function AppText({ children, textType }) {
-  const styles = StyleSheet.create({
-    text: {
-      fontFamily: "Verdana",
-      ...(textType ? textType : texts.body),
-    },
-  });
-  return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, type = texts.body }) {
+  return <Text style={[styles.text, { ...type }]}>{children}</Text>;
 }
+
+const styles = StyleSheet.create({
+  text: {
+    ...texts.body,
+    fontFamily: "Verdana",
+  },
+});
 
 export default AppText;
