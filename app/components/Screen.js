74e3133524aedf661dcb811df9f1446b00709459
Magnus H <me@magnushodne.com>
2022-03-17 13:23:48 +0100

    Added the ListingScreen and made Screen support horizontal padding

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Constants from "expo-constants";
-import { StyleSheet, SafeAreaView } from "react-native";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 
 // Platform independent version of SafeAreaView
 function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      <View style={style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
